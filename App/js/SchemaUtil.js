7a19844c75ed03efcbf5810807565ffd1a996459
Jack Lee <linb.net@gmail.com>
2019-09-16 14:36:36 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/SchemaUtil.js
+++ App/js/SchemaUtil.js
@@ -19,7 +19,66 @@ xui.Class('App.SchemaUtil', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.TreeGrid")
+                .setHost(host,"xui_ui_treegrid32")
+                .setDirtyMark(false)
+                .setLeft("0em")
+                .setTop("0em")
+                .setRowNumbered(true)
+                .setHeader([
+                    {
+                        "id":"col1",
+                        "_cells":{ },
+                        "_serialId":"-h_a",
+                        "_colWidth":"8em",
+                        "_cellWidth":"8em",
+                        "width":"8em",
+                        "_hcellheight":"2em",
+                        "type":"input",
+                        "caption":"col1",
+                        "_region":2
+                    },
+                    {
+                        "id":"col2",
+                        "_cells":{ },
+                        "_serialId":"-h_b",
+                        "_colWidth":"8em",
+                        "_cellWidth":"8em",
+                        "width":"8em",
+                        "_hcellheight":"2em",
+                        "type":"input",
+                        "caption":"col2",
+                        "_region":2
+                    },
+                    {
+                        "id":"col3",
+                        "_cells":{ },
+                        "_serialId":"-h_c",
+                        "_colWidth":"8em",
+                        "_cellWidth":"8em",
+                        "width":"8em",
+                        "_hcellheight":"2em",
+                        "type":"input",
+                        "caption":"col3",
+                        "_region":2
+                    },
+                    {
+                        "id":"col4",
+                        "_cells":{ },
+                        "_serialId":"-h_d",
+                        "_colWidth":"8em",
+                        "_cellWidth":"8em",
+                        "width":"8em",
+                        "_hcellheight":"2em",
+                        "type":"input",
+                        "caption":"col4",
+                        "_region":2
+                    }
+                ])
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
