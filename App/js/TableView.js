7216e8e05c46597f49c199550daed58fc85548e9
Jack Lee <linb.net@gmail.com>
2019-09-17 00:14:44 -0400

    Updated by CrossUI No-Code App Builder

--- App/js/TableView.js
+++ App/js/TableView.js
@@ -19,12 +19,35 @@ xui.Class('App.TableView', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
         // To determine how properties affects this module
         propSetAction : function(prop){
+        },
+        functions:{
+            "setResult":{
+                "desc":"",
+                "params":[
+                    {
+                        "id":"result",
+                        "type":"String",
+                        "desc":""
+                    }
+                ],
+                "actions":[
+                    {
+                        "desc":"log",
+                        "type":"other",
+                        "target":"msg",
+                        "args":[
+                            "{args}"
+                        ],
+                        "method":"log"
+                    }
+                ]
+            }
         }
     },
     // export
