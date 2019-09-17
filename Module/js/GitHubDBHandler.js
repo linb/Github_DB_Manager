5dc0e744e6e75cb68459690fe3fd35d067cb0907
Jack Lee <linb.net@gmail.com>
2019-09-17 00:57:40 -0400

    Updated by CrossUI No-Code App Builder

--- Module/js/GitHubDBHandler.js
+++ Module/js/GitHubDBHandler.js
@@ -352,7 +352,7 @@ xui.Class('Module.GitHubDBHandler', 'xui.Module',{
             // we have to add a search word
             wordIn = wordIn || "//crossui_db_record";
             clientWithAuth.search.code({
-                q: wordIn + "path:"+api.DB_ROOT_PATH+"/"+objectName+"/ extension:json",
+                q: wordIn + " path:"+api.DB_ROOT_PATH+"/"+objectName+"/ extension:json",
                 sort:"indexed",
                 order:order || "desc",
                 page:cur_page|| 1,
